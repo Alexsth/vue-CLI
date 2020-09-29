671566186ffff998603dc20f0af9edec51508311
AlexSth <alexshrestha77@gmail.com>
2020-09-29 11:12:06 +0545

    Components examples

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-  <div>
-  <h1> {{ title }} </h1>
-  <players></players>
-  </div>
+<div>
+  <app-header></app-header>
+  <app-body></app-body>
+  <app-footer></app-footer>
+
+</div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Player from './components/player.vue'
+
 export default {
+  components:{
+    'app-header': Header,
+    "app-footer": Footer,
+    "app-body": Player
+  },
   data () {
     return {
-      title: 'Lets Begin'
+
     }
   }
 }
 </script>
 
-<style scoped>
-h1{
-  color: red;
-}
+<style>
+
 </style>
---
+++ src/components/player.vue
@@ -0,0 +1,55 @@
+<template>
+<div id="players">
+  <ul>
+    <li v-for="player in players" v-on:click="player.show= !player.show">
+    <h2> {{player.name}} </h2>
+    <h3 v-show="player.show"> {{ player.speciality}}</h3>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    return {
+      players: [
+        {name: 'Ronaldo', speciality: 'everything', show: false},
+        {name: 'Messi', speciality: 'dribbling', show: false},
+        {name: 'Ramos', speciality: 'defence', show: false},
+        {name: 'Zidane', speciality: 'Coach', show: false},
+        {name: 'De Gea', speciality: 'Keeper', show: false}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#players{
+  width: 100%;
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+ul{
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+
+li{
+  flex-grow: 1;
+  flex-basis: 300px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+  margin: 10px;
+}
+
+</style>
