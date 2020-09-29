b28387c6724eed9cc8dd7b9f2ea5128a70615a45
AlexSth <alexshrestha77@gmail.com>
2020-09-29 15:57:28 +0545

    add filters

--- src/main.js
+++ src/main.js
@@ -3,27 +3,14 @@ import App from './App.vue'
 import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
-//Custom directive
-Vue.directive('rainbow',{
-  bind(el,binding,vnode){
-    el.style.color='#'+ Math.random().toString().slice(2,8);
-  }
+//filter
+Vue.filter('to-uppercase',function(value){
+ return value.toUpperCase();
 });
 
-Vue.directive('theme',{
-  bind(el, binding, vnode){
-    if(binding.value == 'wide'){
-      el.style.maxWidth = "1200px";
-    } else if (binding.value == 'narrow'){
-      el.style.maxWidth = "560px";
-    }
-    if(binding.arg == 'column'){
-      el.style.background = "#ddd";
-      el.style.padding = '20px';
-    }
-  }
-})
-export const bus = new Vue();
+Vue.filter('snippet', function(value){
+  return value.slice(0,100)+'...';
+});
 
 new Vue({
   el: '#app',
