1433fb823a595509120c3425f7d4cfc466e2178e
AlexSth <alexshrestha77@gmail.com>
2020-09-29 15:06:42 +0545

    added http requests

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="add-blog">
   <h2>Add a new Blog Post:</h2>
-  <form>
+  <form v-if="!submitted">
     <label>Blog Title:</label>
     <input type="text" v-model.lazy="blog.title" required/>
     <label> Blog Content:</label>
@@ -20,7 +20,11 @@
     <select v-model="blog.author">
       <option v-for="author in authors">{{author}}</option>
     </select>
+    <button v-on:click.prevent="post">Add Blog</button>
   </form>
+  <div v-if="submitted">
+    <h3>Thank you</h3>
+  </div>
   <div id="preview">
     <h3>Preview Blog</h3>
     <p>Blog Title:{{blog.title}}</p>
@@ -51,12 +55,22 @@ export default {
       categories:[],
       author:''
       },
-      authors:['Alex Shrestha', 'Rajendra Shrestha']
+      authors:['Alex Shrestha', 'Rajendra Shrestha'],
+      submitted: false
     }
 
   },
   methods: {
-
+    post: function(){
+      this.$http.post('https://jsonplaceholder.typicode.com/posts',{
+        title:this.blog.title,
+        body:this.blog.content,
+        userid: 1
+      }).then(function(data){
+        console.log(data);
+        this.submitted= true;
+      });
+    }
     },
 }
 </script>
