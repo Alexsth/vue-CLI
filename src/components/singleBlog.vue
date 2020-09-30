c4dd2d1890769eeb93e9a96b84571cd873ea62e3
AlexSth <alexshrestha77@gmail.com>
2020-09-30 15:24:21 +0545

    completed hooking up firebase

--- src/components/singleBlog.vue
+++ src/components/singleBlog.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="single-blog">
       <h1>{{blog.title}}</h1>
-      <article>{{blog.body}}</article>
+      <article>{{blog.content}}</article>
+      <p>Author:{{blog.author}}</p>
+      <ul>
+        <li v-for="category in blog.categories">{{category}}</li>
+      </ul>
   </div>
 </template>
 
@@ -14,10 +18,17 @@ export default {
     }
   },
   created(){
-    this.$http.get('https://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data){
+    this.$http.get('https://vuejs-c1f9a.firebaseio.com/posts/' + this.id + '.json').then(function(data){
 
-      this.blog= data.body;
+      return data.json();
+    }).then(function(data){
+      this.blog = data;
     })
   }
 }
 </script>
+<style scoped>
+#single-blog{
+  padding: 10px;
+}
+</style>
